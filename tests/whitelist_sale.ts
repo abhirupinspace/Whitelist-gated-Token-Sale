1127b1866d2fcc0b03921bcadf807575f5205c71
abhirupinspace <abkenway2003@gmail.com>
2024-07-14 21:41:06 +0530

    Your commit message

--- tests/whitelist_sale.ts
+++ tests/whitelist_sale.ts
@@ -1,19 +1,3 @@
-// import * as anchor from "@coral-xyz/anchor";
-// import { Program } from "@coral-xyz/anchor";
-
-// describe("whitelist_sale", () => {
-  
-//   anchor.setProvider(anchor.AnchorProvider.env());
-
-//   const program = anchor.workspace.WhitelistSale as Program<WhitelistSale>;
-
-//   it("Is initialized!", async () => {
-//     // Add your test here.
-//     const tx = await program.methods.initialize().rpc();
-//     console.log("Your transaction signature", tx);
-//   });
-// });
-
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { WhitelistSale } from "../target/types/whitelist_sale";
